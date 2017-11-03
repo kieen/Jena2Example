8aebb2f824f2e3727d66533fcbec01a5395295c4
kieen <kientrantrung.code@gmail.com>
2017-11-03 15:37:06 +0100

    first commit

---
+++ src/main/java/parsing/ParserExample.java
@@ -0,0 +1,21 @@
+package parsing;
+
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.system.StreamRDF;
+
+public class ParserExample {
+
+	public static void main(String[] argvs) {
+		String fileName = "src/main/resources/test1.ttl";
+		StreamRDF tripleHandler = new TripleHandler();
+
+		// Call the parsing process.
+		long start = System.currentTimeMillis();
+		RDFDataMgr.parse(tripleHandler, fileName);
+
+		long end = System.currentTimeMillis();
+		long time = (end - start) / 1000;
+		System.out.println("Parsing time (s): " + time);
+
+	}
+}
